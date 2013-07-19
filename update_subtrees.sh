0b2bcf62311e5c1baca397fa93163e05e745eac3
Josef Weinbub <weinbub@iue.tuwien.ac.at>
2013-07-19 17:18:24 +0200

    addes script which updates all available git subtrees automatically
    otherwise the process is a little painful

---
+++ update_subtrees.sh
@@ -0,0 +1,5 @@
+git subtree pull --prefix external/ViennaMath viennamath master --squash
+git subtree pull --prefix external/ViennaMini viennamini master --squash
+git subtree pull --prefix external/ViennaFVM viennafvm master --squash
+git subtree pull --prefix external/ViennaData viennadata master --squash
+git subtree pull --prefix external/ViennaGrid viennagrid master --squash
